c07756d9c3e0b5a021f78aede58aafa2d0cc8f9f
shristii0304 <shristidubey807@gmail.com>
2025-12-09 05:30:16 +0000

    day 02 task

---
+++ basic/day02.js
@@ -0,0 +1,26 @@
+let a =23;
+console.log(a);
+const b="abc";
+console.log(b);
+var c= 78;
+console.log(c);
+d=88;
+console.log(d);
+
+let e="abc";
+let f="";
+let g=null;
+let h;
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(typeof(e));
+console.log(typeof(undefined));
+console.log(typeof(null));
+
+let score ="33";
+console.log(typeof(score));
+console.log(typeof score);
+let scores=Number(score);
+console.log(typeof(scores));
